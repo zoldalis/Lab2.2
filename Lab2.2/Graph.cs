684b585fde160a39ff3eb63b0ab5987c50d0bf39
дудко михаил <mikel2000@bk.ru>
2020-10-29 17:56:30 +0500

    проблема с f1,f2

---
+++ Lab2.2/Graph.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+namespace Lab2._2
+{
+    public interface Idlls
+    {
+        double f1(double x);
+        string f2();
+    }
+    
+    public class dll1 : Idlls
+    {
+        public double f1(double x) {
+            return 0;
+        }
+        public string f2()
+        {
+            return "";
+        }
+        [DllImport("Lib.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc(double x);
+
+        [DllImport("Lib.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern string FuncName();
+        public dll1()
+        {
+            
+        }
+    }
+    class dll2 : Idlls
+    {
+        public double f1(double x)
+        {
+            return 0;
+        }
+        public string f2()
+        {
+            return "";
+        }
+
+        [DllImport("Lib2-2-1.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc(double x);
+
+        [DllImport("Lib2-2-1.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern string FuncName();
+
+        public dll2()
+        {
+
+        }
+    }
+    class dll3 : Idlls
+    {
+
+        public double f1(double x)
+        {
+            return 0;
+        }
+        public string f2()
+        {
+            return "";
+        }
+        [DllImport("Lib2-2-2.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc(double x);
+
+        [DllImport("Lib2-2-2.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern string FuncName();
+
+        public dll3()
+        {
+
+        }
+    }
+    class dll4 : Idlls
+    {
+
+        public double f1(double x)
+        {
+            return 0;
+        }
+        public string f2()
+        {
+            return "";
+        }
+
+        [DllImport("Lib2-2-3.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc(double x);
+
+        [DllImport("Lib2-2-3.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern string FuncName();
+
+        public dll4()
+        {
+
+        }
+    }
+    class dll5 : Idlls
+    {
+
+        public double f1(double x)
+        {
+            return 0;
+        }
+        public string f2()
+        {
+            return "";
+        }
+
+        [DllImport("Lib2-2-3-1.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc(double x);
+
+        [DllImport("Lib2-2-3-1.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern string FuncName();
+
+        public dll5()
+        {
+
+        }
+    }
+    class dll6 : Idlls
+    {
+
+        public double f1(double x)
+        {
+            return 0;
+        }
+        public string f2()
+        {
+            return "";
+        }
+
+        [DllImport("Lib2-2-3-2.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc(double x);
+
+        [DllImport("Lib2-2-3-2.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern string FuncName();
+
+        public dll6()
+        {
+
+        }
+
+    }
+    class Graph
+    {
+
+
+        public Graph()
+        {
+
+        }
+        public void Draw(double[] nums)
+        {
+            try
+            {
+                Application application = new Application();
+                application.Visible = true;
+                Workbook workbook = application.Workbooks.Add(1);
+                Worksheet sheet = (Worksheet)workbook.ActiveSheet;
+                int i = 0;
+                for (; i < 10; i++)
+                {
+                    sheet.Cells[i + 1, 1] = i;
+                    double f = nums[i];
+                    sheet.Cells[i + 1, 2] = f;
+                }
+                ChartObjects xlCharts = (ChartObjects)sheet.ChartObjects();
+                ChartObject myChart = (ChartObject)xlCharts.Add(110, 0, 350, 250);
+                Chart chart = myChart.Chart;
+                SeriesCollection seriesCollection = (SeriesCollection)chart.SeriesCollection();
+                Series series = seriesCollection.NewSeries();
+                string A = "A" + Convert.ToString(i + 1);
+                string B = "B" + Convert.ToString(i + 1);
+                series.XValues = sheet.get_Range("A1", A);
+                series.Values = sheet.get_Range("B1", B);
+                chart.ChartType = XlChartType.xlXYScatterSmooth;
+            }
+            catch (Exception e)
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
---
+++ Lab2.2/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._2
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Graph gr = new Graph();
+            List<Idlls> dlls = new List<Idlls>();
+            dlls.Add(new dll1());
+            dlls.Add(new dll2());
+            dlls.Add(new dll3());
+            dlls.Add(new dll4());
+            dlls.Add(new dll5());
+            dlls.Add(new dll6());
+            double[] nums = new double[10];
+            foreach (var item in dlls)
+            {
+                Console.WriteLine("Введите номер функции которую нужно посчитать?: TheFunc(1) ; FuncName(2)");
+                string s = Console.ReadLine();
+                if (s == "1")
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        nums[i] = item.f1((double)i);
+                    }
+                    gr.Draw(nums);
+                }
+                else
+                {
+                    Console.WriteLine(item.f2());
+                }
+                
+            }
+
+            //Graph gr = new Graph();
+            //Console.WriteLine("Введите номер функции которую нужно посчитать?: TheFunc(1) ; FuncName(2)");
+           // string s = Console.ReadLine();
+            //    if (s == "1")
+            //    {
+            //        try
+            //        {
+            //            for (int i = 0; i < 10; i++)
+            //            {
+            //                nums[i] = dll1.TheFunc(i);
+            //            }
+            //            gr.Draw(nums);
+            //        }
+            //        catch (Exception)
+            //        {
+            //            try
+            //            {
+            //                for (int i = 0; i < 10; i++)
+            //                {
+            //                    nums[i] = dll2.TheFunc(i);
+            //                }
+            //                gr.Draw(nums);
+            //            }
+            //            catch (Exception)
+            //            {
+            //                try
+            //                {
+            //                    for (int i = 0; i < 10; i++)
+            //                    {
+            //                        nums[i] = dll3.TheFunc(i);
+            //                    }
+            //                    gr.Draw(nums);
+            //                }
+            //                catch (Exception)
+            //                {
+            //                    try
+            //                    {
+            //                        for (int i = 0; i < 10; i++)
+            //                        {
+            //                            nums[i] = dll4.TheFunc(i);
+            //                        }
+            //                        gr.Draw(nums);
+            //                    }
+            //                    catch (Exception)
+            //                    {
+            //                        try
+            //                        {
+            //                            for (int i = 0; i < 10; i++)
+            //                            {
+            //                                nums[i] = dll5.TheFunc(i);
+            //                            }
+            //                            gr.Draw(nums);
+            //                        }
+            //                        catch (Exception)
+            //                        {
+            //                            for (int i = 0; i < 10; i++)
+            //                            {
+            //                                nums[i] = dll6.TheFunc(i);
+            //                            }
+            //                            gr.Draw(nums);
+            //                            throw;
+            //                        }
+            //                        throw;
+            //                    }
+            //                    throw;
+            //                }
+            //                throw;
+            //            }
+            //            throw;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        try
+            //        {
+
+            //            Console.WriteLine(dll1.FuncName());
+            //            Console.WriteLine("1");
+            //        }
+            //        catch (Exception)
+            //        {
+            //            try
+            //            {
+            //                Console.WriteLine(dll2.FuncName());
+            //                Console.WriteLine("2");
+
+            //            }
+            //            catch (Exception)
+            //            {
+            //                try
+            //                {
+            //                    Console.WriteLine(dll3.FuncName());
+            //                    Console.WriteLine("3");
+
+            //                }
+            //                catch (Exception)
+            //                {
+            //                    try
+            //                    {
+            //                        Console.WriteLine(dll4.FuncName());
+            //                        Console.WriteLine("4");
+
+            //                    }
+            //                    catch (Exception)
+            //                    {
+            //                        try
+            //                        {
+            //                            Console.WriteLine(dll5.FuncName());
+            //                            Console.WriteLine("5");
+
+            //                        }
+            //                        catch (Exception)
+            //                        {
+            //                            Console.WriteLine(dll6.FuncName());
+            //                            Console.WriteLine("6");
+
+            //                        }
+            //                        throw;
+            //                    }
+            //                    throw;
+            //                }
+            //                throw;
+            //            }
+            //            throw;
+            //        }
+            //    }
+
+
+        }  
+
+        }
+}
+
